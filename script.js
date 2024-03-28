e120d51ba9b02af72f94d138c58ed4222516f0f5
Jakub Molinski <jakubmolinski9@gmail.com>
2024-03-28 15:35:57 +0100

    Working solution for array methods first exercise not included in assignments

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const sumOfTripledEvens = (array) => {
-  const mappedArray = array.map((number) => {
-    if (number % 2 === 0) {
-      number *= 3;
-      console.log(number);
-    }
-  });
-
-  return mappedArray;
+const sumOfTripledEvens = (arr) => {
+  return arr
+    .filter((number) => number % 2 === 0)
+    .map((number) => number * 3)
+    .filter((acc, current) => acc + current);
 };
 
 sumOfTripledEvens(arr);
